f0249fb88d58f7d9df2e8d42ed86f1419ea4ca6e
niranjan <niranjan64461231@gmail.com>
2025-07-18 01:17:50 +0530

    first commit

---
+++ day9/style/try.js
@@ -0,0 +1,14 @@
+let text=document.querySelectorAll("p");
+
+while(true)
+{
+    if(text.stye.backgroundColor=="black")
+   {text.stye.backgroundColor="white";
+    text.stye.color="black";
+   }
+   else{
+    text.stye.backgroundColor ="black";
+    text.stye.color="white";
+   }
+
+}
